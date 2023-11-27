4a02a0932fe1f33502b43fb31973f8ec98c9c0cc
alejopua <alejopua@gmail.com>
2023-11-27 18:19:29 -0500

    [add](class): class 5 - boolean

---
+++ src/basics/05-boolean.ts
@@ -0,0 +1,17 @@
+(() => {
+    let isNumber = true;
+    isNumber = 1;
+    console.log(isNumber);
+    
+    let isGreaterThan: boolean = false;
+    console.log(isGreaterThan);
+
+    let random = Math.random();
+    console.log(random);
+
+    isGreaterThan = (random > 0.5 ? true : false);
+    console.log(isGreaterThan);
+
+    const isBoolean: boolean = false;
+    console.log(isBoolean);
+})();
