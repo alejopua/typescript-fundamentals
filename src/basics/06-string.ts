b8120a9141edb5003ba6f07ce447ccc8ff55884f
alejopua <alejopua@gmail.com>
2023-11-27 18:19:45 -0500

    [add](class): class 6 - string

---
+++ src/basics/06-string.ts
@@ -0,0 +1,7 @@
+(() => {
+    let myName: string = 'John';
+    let lastName = 'Doe';
+    const other = 'Other';
+    let message = `Hello ${myName} ${lastName}, ${other}`;
+    console.log(message); // Hello John
+})();
